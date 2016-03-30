9b611d8ed81d148638a7e86c945ccb01d48adaf1
Juan Ignacio Fernández Díez <sr.jifernandez@gmail.com>
2016-03-30 21:23:18 +0200

    Change Message

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -2,6 +2,6 @@ import React from 'react';
 
 export default class Header extends React.Component {
   render () {
-    return <h1>Hello <strong>{this.props.name}</strong>, this is my first isomorphic app done in ES6</h1>
+    return <h1>Hi <strong>{this.props.name}</strong>, this is a very easy example of isomorphic app done in ES6</h1>
   }
 }
