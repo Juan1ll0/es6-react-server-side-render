dc740a43de916be8b71bf00e3ad126ca02ffcabf
Juan Ignacio Fernández Díez <sr.jifernandez@gmail.com>
2016-03-31 22:29:50 +0200

    Adds /:name route

--- routes/main-routes.js
+++ routes/main-routes.js
@@ -8,8 +8,16 @@ import Home from '../src/pages/home';
 
 const router = express.Router();
 
-// Export object
-export default router.get('/', (req, res) => {
+// Home route
+router.get('/', (req, res) => {
   let html = ReactDOMServer.renderToString(<Home name='User' />);
   res.end('<!DOCTYPE html>' + html);
 });
+
+// Name route
+router.get('/:name', (req, res) => {
+  let html = ReactDOMServer.renderToString(<Home name={req.params.name} />);
+  res.end('<!DOCTYPE html>' + html);
+});
+
+export default router;
