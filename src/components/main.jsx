a835c6e96433985e19664040a7ec89f785d578b5
Juan Ignacio Fernández Díez <sr.jifernandez@gmail.com>
2016-04-04 22:20:53 +0200

    Change main to stateless component. Minor changes in layout.

--- src/components/main.jsx
+++ src/components/main.jsx
@@ -1,24 +1,11 @@
 import React from 'react';
 
 export default class Main extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.onClickButton = this.onClickButton.bind(this);
-    this.state = {
-      counter: parseInt(props.counter)
-    };
-  }
-
-  onClickButton () {
-    this.setState({ counter: this.state.counter += 1 });
-  }
-
   render () {
     return (
       <section>
-        <button onClick={this.onClickButton}>Click ME!!!</button>
-        <span> {this.state.counter} Clicks</span>
+        <button onClick={this.props.onClickCallback}>Click ME!!!</button>
+        <span> {this.props.counter} Clicks</span>
       </section>
     );
   }
