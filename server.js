9a46b9f797bc177132d77665e24a8ba9f5907af6
Juan Ignacio Fernández Díez <sr.jifernandez@gmail.com>
2016-04-04 22:19:59 +0200

    Include serve-favicon in server

--- server.js
+++ server.js
@@ -1,17 +1,22 @@
 // Dependencies
 import express from 'express';
 import path from 'path';
+import favicon from 'serve-favicon';
 
 // Routes
 import routes from './routes/main-routes';
 
 // consts
-const app = express();
+const app = express(),
+      faviconURL = __dirname + '/public/img/favicon.ico';
 
 // Set view engine to jade
 app.set('views', path.join(__dirname, 'views'));
 app.set('view options', { pretty: true })
 app.set('view engine','jade');
+
+// Favicon
+app.use(favicon(faviconURL));
 
 // Middlewares
 app.use(express.static(path.join(__dirname + '/public')));
