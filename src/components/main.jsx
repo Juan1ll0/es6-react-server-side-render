46a99a2d26f0579a61b8c03d7a0383143a2a81bb
Juan Ignacio Fernández Díez <sr.jifernandez@gmail.com>
2016-04-07 23:32:18 +0200

    Dispatch incCounter action

--- src/components/main.jsx
+++ src/components/main.jsx
@@ -1,10 +1,16 @@
 import React from 'react';
 
+import AppActions from '../actions/actions';
+
 export default class Main extends React.Component {
+  incCounter() {
+    AppActions.incCounter();
+  }
+
   render () {
     return (
       <section>
-        <button onClick={this.props.onClickCallback}>Click ME!!!</button>
+        <button onClick={this.incCounter.bind(this)}>Click ME!!!</button>
         <span> {this.props.counter} Clicks</span>
       </section>
     );
