9dddd44dbd72e332917dfd9989c679271624176a
Juan Ignacio Fernández Díez <sr.jifernandez@gmail.com>
2016-04-09 11:01:52 +0200

    Change style and adds section tag

--- src/components/list.jsx
+++ src/components/list.jsx
@@ -1,9 +1,8 @@
 import React from 'react';
 
 const style = {
-  width: '30%',
-  margin: '2px',
-  display: 'inline-block',
+  width: '50%',
+  margin: '4px',
   textDecoration: 'none',
   color: 'white'
 }
@@ -11,13 +10,15 @@ const style = {
 export default class ListColors extends React.Component {
   render () {
     return (
-      <ul>
-        {
-          this.props.colors.map((color, index) => {
-            return <li key={index} style={Object.assign({}, style, {backgroundColor: color})}>{color}</li>
-          })
-        }
-      </ul>
+      <section>
+        <ul>
+          {
+            this.props.colors.map((color, index) => {
+              return <li key={index} style={Object.assign({}, style, {backgroundColor: color})}>{color}</li>
+            })
+          }
+        </ul>
+      </section>
     );
   }
 };
