fbd6680696073fdf61c66b08c44b4db5ee856c99
Juan Ignacio Fernández Díez <sr.jifernandez@gmail.com>
2016-05-08 14:25:31 +0200

    Clean server code

--- server.js
+++ server.js
@@ -1,23 +1,41 @@
 // Dependencies
 import express from 'express';
+import favicon from 'serve-favicon';
+import React from 'react';
+import ReactDOMServer from 'react-dom/server';
+import { match, RouterContext, browserHistory } from 'react-router';
 
 // Routes
-import routes from './routes/main-routes';
+import routes from './router/routes';
+
+// Components
+import App from './src/app';
+import Home from './src/components/home';
+import Users from './src/components/users';
 
 // consts
-const app = express();
+const app = express(),
+			faviconURL = __dirname + '/public/img/favicon.ico';
 
-// Set public path
+// Setup App
 app.use(express.static(__dirname + '/public'));
+app.use(favicon(faviconURL));
 
-// Route /
-app.use('/', routes);
+// Start App
+app.use((req, res, next) => {
+
+	// Create Router
+  match({ routes, location: req.url, history: browserHistory }, (error, redirect, renderProps) => {
+      if (error)
+        res.status(500).send(error.message);
+      else {
+        let html = ReactDOMServer.renderToString(<RouterContext {...renderProps} />);
+        res.end('<!DOCTYPE html>' + html);
+      }
+  });
+});
 
 // Start server
-const server = app.listen(3000, () => {
-	// Extract port
-  const {port} = server.address();
-
-  // string interpolation:
-  console.log(`React ES6 Server Side Render app listening at http://localhost:${port}`);
+app.listen(3000, () => {
+  console.log('React ES6 Server Side Render app listening at http://localhost:3000');
 });
