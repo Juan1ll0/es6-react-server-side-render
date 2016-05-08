0440d808eee8156743a9ffca54ec94ee599a23af
Juan Ignacio Fernández Díez <sr.jifernandez@gmail.com>
2016-05-08 14:24:43 +0200

    Add new and clean components

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
 
-export default class Header extends React.Component {
-  render () {
-    return <h1>Hi <strong>{this.props.name}</strong>, this is a very easy example of isomorphic app done in ES6</h1>
-  }
-}
+const header = () =>
+  <header>
+    <h1>Basic example of isomorphic app done in ES6</h1>
+  </header>
+
+export default header;
