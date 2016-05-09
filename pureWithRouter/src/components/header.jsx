91423b2a7103494752e129251fdb77da6d38d9ee
Juan Ignacio Fernández Díez <sr.jifernandez@gmail.com>
2016-05-09 19:35:33 +0200

    Pure React App using react-router

---
+++ pureWithRouter/src/components/header.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+
+const header = () =>
+  <header>
+    <h1>Basic example of isomorphic app done in ES6</h1>
+  </header>
+
+export default header;
---
+++ pureWithRouter/src/components/users.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const users = () =>
+  <div>
+    <h3>Users List</h3>
+    <ul>
+      <li>Peter</li>
+      <li>John</li>
+      <li>Jack</li>
+    </ul>
+  </div>
+
+export default users;
